7c6cff77d0ec746d1490f2b222a538e17f4dc550
Restyled.io <commits@restyled.io>
2020-01-29 15:00:45 +0000

    Restyled by clang-format

--- macosx/HBQueueInfoViewController.m
+++ macosx/HBQueueInfoViewController.m
@@ -9,137 +9,126 @@
 
 @interface HBQueueInfoViewController ()
 
-@property (nonatomic, weak) IBOutlet NSView *statisticsHeader;
-@property (nonatomic, weak) IBOutlet NSTextField *statisticsLabel;
-@property (nonatomic, weak) IBOutlet NSTextField *summaryLabel;
-@property (nonatomic, weak) IBOutlet NSScrollView *scrollView;
-@property (nonatomic, weak) IBOutlet NSBox *divider;
+@property(nonatomic, weak) IBOutlet NSView *statisticsHeader;
+@property(nonatomic, weak) IBOutlet NSTextField *statisticsLabel;
+@property(nonatomic, weak) IBOutlet NSTextField *summaryLabel;
+@property(nonatomic, weak) IBOutlet NSScrollView *scrollView;
+@property(nonatomic, weak) IBOutlet NSBox *divider;
 
-@property (nonatomic, weak) id<HBQueueDetailsViewControllerDelegate> delegate;
+@property(nonatomic, weak) id<HBQueueDetailsViewControllerDelegate> delegate;
 
-@property (nonatomic) BOOL canReset;
+@property(nonatomic) BOOL canReset;
 
 @end
 
 @implementation HBQueueInfoViewController
 
-- (instancetype)initWithDelegate:(id<HBQueueDetailsViewControllerDelegate>)delegate
-{
-    self = [super init];
-    if (self)
-    {
-        _delegate = delegate;
-    }
-    return self;
+- (instancetype)initWithDelegate:
+    (id<HBQueueDetailsViewControllerDelegate>)delegate {
+  self = [super init];
+  if (self) {
+    _delegate = delegate;
+  }
+  return self;
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    [self updateLabels];
-    [self setUpObservers];
+  [super viewDidLoad];
+  [self updateLabels];
+  [self setUpObservers];
 }
 
-- (void)setUpObservers
-{
-    // It would be easier to just KVO the item state property,
-    // But we can't because the item is a NSProxy.
-    NSNotificationCenter * __weak center = NSNotificationCenter.defaultCenter;
+- (void)setUpObservers {
+  // It would be easier to just KVO the item state property,
+  // But we can't because the item is a NSProxy.
+  NSNotificationCenter *__weak center = NSNotificationCenter.defaultCenter;
 
-    [center addObserverForName:HBQueueDidStartItemNotification
-            object:nil
-            queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification * _Nonnull note)
-           {
-               HBQueueItem *startedItem = note.userInfo[HBQueueItemNotificationItemKey];
+  [center addObserverForName:HBQueueDidStartItemNotification
+                      object:nil
+                       queue:NSOperationQueue.mainQueue
+                  usingBlock:^(NSNotification *_Nonnull note) {
+                    HBQueueItem *startedItem =
+                        note.userInfo[HBQueueItemNotificationItemKey];
 
-               if (startedItem == self.item)
-        {
-            [self updateUI];
-        }
-    }];
+                    if (startedItem == self.item) {
+                      [self updateUI];
+                    }
+                  }];
 
-    [center addObserverForName:HBQueueDidCompleteItemNotification
-            object:nil
-            queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification * _Nonnull note)
-           {
-               HBQueueItem *completedItem = note.userInfo[HBQueueItemNotificationItemKey];
+  [center addObserverForName:HBQueueDidCompleteItemNotification
+                      object:nil
+                       queue:NSOperationQueue.mainQueue
+                  usingBlock:^(NSNotification *_Nonnull note) {
+                    HBQueueItem *completedItem =
+                        note.userInfo[HBQueueItemNotificationItemKey];
 
-               if (completedItem == self.item)
-        {
-            [self updateUI];
-        }
-    }];
+                    if (completedItem == self.item) {
+                      [self updateUI];
+                    }
+                  }];
 
-    [center addObserverForName:HBQueueDidChangeItemNotification
-            object:nil
-            queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification * _Nonnull note)
-           {
-               [self updateUI];
-           }];
-
+  [center addObserverForName:HBQueueDidChangeItemNotification
+                      object:nil
+                       queue:NSOperationQueue.mainQueue
+                  usingBlock:^(NSNotification *_Nonnull note) {
+                    [self updateUI];
+                  }];
 }
 
-- (void)updateUI
-{
-    [self updateLabels];
-    [self updateReset];
-    dispatch_async(dispatch_get_main_queue(), ^ {
-        [self updateDivider];
-    });
-}
-
-- (void)updateReset
-{
-    self.canReset = self.item && (self.item.state != HBQueueItemStateWorking && self.item.state != HBQueueItemStateReady);
-}
-
-- (void)updateLabels
-{
-    if (self.item)
-    {
-        self.statisticsLabel.hidden = self.item.startedDate == nil;
-        self.statisticsHeader.hidden = self.item.startedDate == nil;
-        self.summaryLabel.hidden = NO;
-
-        self.statisticsLabel.attributedStringValue = self.item.attributedStatistics;
-        self.summaryLabel.attributedStringValue = self.item.attributedDescription;
-
-        dispatch_async(dispatch_get_main_queue(), ^ {
-            [self.scrollView flashScrollers];
-        });
-    }
-    else
-    {
-        self.statisticsHeader.hidden = YES;
-        self.statisticsLabel.hidden = YES;
-        self.summaryLabel.hidden = YES;
-    }
-}
-
-- (void)updateDivider
-{
-    self.divider.hidden = self.scrollView.frame.size.height > self.scrollView.contentView.documentView.frame.size.height;
-}
-
-- (void)viewDidLayout
-{
-    [super viewDidLayout];
+- (void)updateUI {
+  [self updateLabels];
+  [self updateReset];
+  dispatch_async(dispatch_get_main_queue(), ^{
     [self updateDivider];
+  });
 }
 
-- (void)setItem:(HBQueueItem *)item
-{
-    _item = item;
-    [self updateUI];
+- (void)updateReset {
+  self.canReset = self.item && (self.item.state != HBQueueItemStateWorking &&
+                                self.item.state != HBQueueItemStateReady);
 }
 
-- (IBAction)editItem:(id)sender
-{
-    [self.delegate detailsViewEditItem:self.item];
+- (void)updateLabels {
+  if (self.item) {
+    self.statisticsLabel.hidden = self.item.startedDate == nil;
+    self.statisticsHeader.hidden = self.item.startedDate == nil;
+    self.summaryLabel.hidden = NO;
+
+    self.statisticsLabel.attributedStringValue = self.item.attributedStatistics;
+    self.summaryLabel.attributedStringValue = self.item.attributedDescription;
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [self.scrollView flashScrollers];
+    });
+  } else {
+    self.statisticsHeader.hidden = YES;
+    self.statisticsLabel.hidden = YES;
+    self.summaryLabel.hidden = YES;
+  }
 }
 
-- (IBAction)resetItem:(id)sender
-{
-    [self.delegate detailsViewResetItem:self.item];
+- (void)updateDivider {
+  self.divider.hidden =
+      self.scrollView.frame.size.height >
+      self.scrollView.contentView.documentView.frame.size.height;
+}
+
+- (void)viewDidLayout {
+  [super viewDidLayout];
+  [self updateDivider];
+}
+
+- (void)setItem:(HBQueueItem *)item {
+  _item = item;
+  [self updateUI];
+}
+
+- (IBAction)editItem:(id)sender {
+  [self.delegate detailsViewEditItem:self.item];
+}
+
+- (IBAction)resetItem:(id)sender {
+  [self.delegate detailsViewResetItem:self.item];
 }
 
 @end
